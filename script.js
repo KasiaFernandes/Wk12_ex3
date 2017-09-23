5e0146d7b05ddbda9102d4bc405351e31c0904e2
Kasia Fernandes <fernandes.kasia@gmail.com>
2017-09-23 09:24:30 +0200

    Code changes

--- script.js
+++ script.js
@@ -5,17 +5,28 @@ $("#search").click(searchCountries);
 
 function searchCountries() {
     var countryName = $("#country-name").val();
-    if(!countryName.length) countryName = "Poland";
+    if(!countryName.length) {countryName = "Poland";
+    }
     $.ajax({
         url: url + countryName,
         method: "GET",
-        success: showCountriesList
+        success: showCountriesList,
+        error: showError
     });
 }
 
 function showCountriesList(resp) {
     countriesList.empty();
     resp.forEach(function(item) {
-        $("<li>").text(item.name).appendTo(countriesList);
+        $("<li>").text("Country name: " + item.name).appendTo(countriesList);
+        $("<li>").text("Capital City: " + item.capital).appendTo(countriesList);
+        $("<li>").text("Belongs to the following region: " + item.region).appendTo(countriesList);
     });
+}
+
+function showError(xhr, status, errorThrown) {
+    countriesList.empty();
+    if(xhr.status == 404) {
+        $("<p>").text("Ooops! Something went wrong. Please, enter a valid country name!").appendTo(countriesList);
+    }
 }
